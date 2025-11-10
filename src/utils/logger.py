7d514b0178bf2e14acc8a3576c81b19b48e243e4
Anni Bamwenda <67605413+Anni-Bamwenda@users.noreply.github.com>
2025-11-10 14:23:35 -0500

    Add logger setup utility function

---
+++ src/utils/logger.py
@@ -0,0 +1,11 @@
+import logging
+
+def setup_logger(name="email_optimizer"):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    if not logger.handlers:
+        ch = logging.StreamHandler()
+        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
+    return logger
